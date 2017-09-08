50ba142cd0482387929efff69883ab9768d68423
huangke19 <huangkwell@163.com>
2017-09-08 13:26:54 +0800

    初步成型

---
+++ K_Movies/映射表.py
@@ -0,0 +1,76 @@
+
+film_type_table = {
+    'action': 2,
+    'comedy': 14,
+    'love': 7,
+    'war': 15,
+    'science': 3,
+    'story': 8,
+    'history': 16,
+    'sordsman': 31,
+    'panic': 10,
+    'suspense': 19,
+    'crime': 11,
+    'disaster': 33,
+    'dance': 13,
+    'all': 21,
+
+}
+
+
+film_country_table = {
+    'all': 0,
+    'China': 6,
+    'USA': 1,
+    'France': 51,
+    # 'UK': 91,
+    'Japan': 4,
+    'Korea': 2,
+    'India': 3,
+    'Thailand': 7,
+    'HongKong': 5,
+    'Taiwan': 8,
+    'else': 14,
+}
+
+#
+# tv_type = {
+#     'city': '都市',
+#     'family': '家庭',
+#     'idol': '偶像',
+#     'comedy': '喜剧',
+#     'period': '古装',
+#     'crimal': '刑侦',
+#     'myth': '神话',
+#     'love': '爱情',
+#     'feature': '剧情',
+#     'campus': '校园',
+#     'cross': '穿越',
+#     'suspense': '悬疑',
+#     'crime': '犯罪',
+#     'youth': '青春',
+#     'motivational': '励志',
+#     'comic':'动画',
+#     'comic2':'动漫',
+#     'action':'动作',
+#     'variety':'综艺',
+#     'tvs':'电视剧',
+#     'all': 0,
+# }
+#
+# tv_area = {
+#     'all': 0,
+#     'cn': u'大陆',
+#     'usa': u'美国',
+#     'france': u'法国',
+#     'en': u'英国',
+#     'japan': u'日本',
+#     'korea': u'韩国',
+#     'india': u'印度',
+#     'thailand': u'泰国',
+#     'hk': u'香港',
+#     'taiwan': u'台湾',
+#     'germany': u'德国',
+#     'else': 1
+# }
+
