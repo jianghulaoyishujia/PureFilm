c29e7a08356c6e292fe54384e6c144e702c40570
huangke19 <huangkwell@163.com>
2017-09-10 23:55:04 +0800

    mysql

--- film/migrations/0001_initial.py
+++ film/migrations/0001_initial.py
@@ -13,40 +13,40 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ACTOR',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200, null=True)),
             ],
             options={
-                'verbose_name_plural': '演员',
                 'verbose_name': '演员',
+                'verbose_name_plural': '演员',
             },
         ),
         migrations.CreateModel(
             name='COUNTRY',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200, null=True)),
             ],
             options={
-                'verbose_name_plural': '国家',
                 'verbose_name': '国家',
+                'verbose_name_plural': '国家',
             },
         ),
         migrations.CreateModel(
             name='DIRECTOR',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200, null=True)),
             ],
             options={
-                'verbose_name_plural': '导演',
                 'verbose_name': '导演',
+                'verbose_name_plural': '导演',
             },
         ),
         migrations.CreateModel(
             name='FILM',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200, verbose_name='电影名称', null=True)),
                 ('language', models.CharField(max_length=200, verbose_name='语言', null=True)),
                 ('IMDb_link', models.CharField(max_length=200, verbose_name='IMDB链接', null=True)),
@@ -54,46 +54,46 @@ class Migration(migrations.Migration):
                 ('by_name', models.CharField(max_length=500, verbose_name='链接详情', null=True)),
                 ('by_link', models.CharField(max_length=500, verbose_name='下载链接1', null=True)),
                 ('pic_link', models.CharField(max_length=500, verbose_name='图片链接', null=True)),
-                ('actor', models.ManyToManyField(to='film.ACTOR', verbose_name='演员', related_name='actor_films')),
-                ('country', models.ForeignKey(null=True, to='film.COUNTRY', verbose_name='国家')),
-                ('director', models.ManyToManyField(to='film.DIRECTOR', verbose_name='导演', related_name='director_films')),
+                ('actor', models.ManyToManyField(to='film.ACTOR', related_name='actor_films', verbose_name='演员')),
+                ('country', models.ForeignKey(to='film.COUNTRY', verbose_name='国家', null=True)),
+                ('director', models.ManyToManyField(to='film.DIRECTOR', related_name='director_films', verbose_name='导演')),
             ],
             options={
                 'verbose_name': '电影',
-                'verbose_name_plural': '电影',
                 'ordering': ('name',),
+                'verbose_name_plural': '电影',
             },
         ),
         migrations.CreateModel(
             name='TYPE',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200, null=True)),
             ],
             options={
-                'verbose_name_plural': '类型',
                 'verbose_name': '类型',
+                'verbose_name_plural': '类型',
             },
         ),
         migrations.CreateModel(
             name='YEAR',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200, null=True)),
             ],
             options={
-                'verbose_name_plural': '年份',
                 'verbose_name': '年份',
+                'verbose_name_plural': '年份',
             },
         ),
         migrations.AddField(
             model_name='film',
             name='publish_year',
-            field=models.ForeignKey(null=True, to='film.YEAR', verbose_name='上映年份'),
+            field=models.ForeignKey(to='film.YEAR', verbose_name='上映年份', null=True),
         ),
         migrations.AddField(
             model_name='film',
             name='types',
-            field=models.ManyToManyField(to='film.TYPE', verbose_name='类型', related_name='films'),
+            field=models.ManyToManyField(to='film.TYPE', related_name='films', verbose_name='类型'),
         ),
     ]
