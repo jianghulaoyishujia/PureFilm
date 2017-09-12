d8748112652a7d2e85e60229e7d5577265201ce4
huangke19 <huangkwell@163.com>
2017-09-12 13:21:55 +0800

    添加了新的电影

--- K_Movies/settings.py
+++ K_Movies/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'doyr4kbn95!h^2-k!pq+b8gk+pp$krxl(xr1z+gyiwxfp+z0^s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.6.104',
 		 '192,168.6.100',
